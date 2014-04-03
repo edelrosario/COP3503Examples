79ca6d5b47d234a7c8aa53c8fedef524c500e60d
Elaine Del Rosario <e.delrosario94@gmail.com>
2014-04-03 09:08:05 -0400

    added comment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//adding a comment
 int main() {
 	std::cout << "We are now compiling with a makefile" << std::endl;
 	std::cout << " This is a command line program " << std:: endl;
