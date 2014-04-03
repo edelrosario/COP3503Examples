d01238d6302b98286053ec9d029da15348a5b5b5
Elaine Del Rosario <e.delrosario94@gmail.com>
2014-04-03 08:53:52 -0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "We are now compiling with a makefile" << std::endl;
+	std::cout << " This is a command line program " << std:: endl;
+	return 0;
+
+}
